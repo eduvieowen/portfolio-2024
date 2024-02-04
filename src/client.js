7764a1945c19df739f366a7ab8046069d2a3eb21
eduvieowen <eduvieouu@gmail.com>
2024-02-04 22:59:41 +0100

    Client side update

--- src/client.js
+++ src/client.js
@@ -2,11 +2,11 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from '@sanity/image-url';
 
 export const client = createClient({
-    projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production',
     aprVersion: '2024-17-01',
     useCdn: true,
-    token: process.env.REACT_APP_SANITY_TOKEN,
+    token: process.env.SANITY_TOKEN,
 });
 
 const builder = imageUrlBuilder(client);
