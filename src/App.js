80de01e6b1ca7151bc3fcc89737c0c771f078299
eduvieowen <eduvieouu@gmail.com>
2024-02-11 14:55:43 +0100

    Anv Hiden

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ const App = () => {
         <Skills />
         <Testimonials />
         <Footer />
-        <Anniversary />
+        {/* <Anniversary /> */}
       </div>
     </ThemeContext.Provider>
   );
